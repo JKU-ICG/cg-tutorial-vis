9052a776caf56fb8da6270d4c903d2b507c2a62d
Holger Stitz <holger.stitz@jku.at>
2018-07-27 16:50:00 +0200

    Initial commit

---
+++ src/views/Lab1.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>Lab1</h1>
+    <HelloWorld msg="Hello Lab1"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class Lab1 extends Vue {}
+</script>
---
+++ src/views/Lab2.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="lab lab2">
+    <h1>Lab2</h1>
+    <HelloWorld msg="Hello Lab2"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class Lab2 extends Vue {}
+</script>
