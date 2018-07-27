9030307d2df81a5d24658b5136494cbf6f3388c1
Holger Stitz <holger.stitz@jku.at>
2018-07-27 18:08:53 +0200

    Add vue-threejs with example component

---
+++ src/components/ThreeJSExample.vue
@@ -0,0 +1,23 @@
+<template>
+  <renderer :size="{ w: 600, h: 400 }">
+    <scene>
+      <camera :position="{ z: 15 }"></camera>
+      <cube texture="cobblestone" :size="1"></cube>
+    </scene>
+  </renderer>
+</template>
+
+<script lang="ts">
+import VueThreejs from "vue-threejs";
+import { Component, Prop, Vue } from "vue-property-decorator";
+import Cube from "@/components/Cube.vue";
+
+Vue.use(VueThreejs);
+
+@Component({
+  components: {
+    Cube
+  }
+})
+export default class ThreeJSExample extends Vue {}
+</script>
--- src/views/Lab1.vue
+++ src/views/Lab1.vue
@@ -2,16 +2,19 @@
   <div class="about">
     <h1>Lab1</h1>
     <CounterControls/>
+    <ThreeJSExample/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import CounterControls from "@/components/CounterControls.vue"; // @ is an alias to /src
+import ThreeJSExample from "@/components/ThreeJSExample.vue";
 
 @Component({
   components: {
-    CounterControls
+    CounterControls,
+    ThreeJSExample
   }
 })
 export default class Lab1 extends Vue {}
