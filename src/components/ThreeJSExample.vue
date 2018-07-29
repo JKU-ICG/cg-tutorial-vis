406ccbdf40fdd0a0029d5a033446eb12db79da10
Holger Stitz <holger.stitz@jku.at>
2018-07-29 23:32:35 +0200

    Experiment with dynamic size property

--- src/components/ThreeJSExample.vue
+++ src/components/ThreeJSExample.vue
@@ -2,7 +2,7 @@
   <renderer :size="{ w: 600, h: 400 }">
     <scene>
       <camera :position="{ z: 15 }"></camera>
-      <cube texture="cobblestone" :size="1"></cube>
+      <cube texture="cobblestone" :size="count"></cube>
     </scene>
   </renderer>
 </template>
@@ -11,12 +11,18 @@
 import VueThreejs from "vue-threejs";
 import { Component, Prop, Vue } from "vue-property-decorator";
 import Cube from "@/components/Cube.vue";
+import { createNamespacedHelpers } from "vuex";
+
+const { mapGetters } = createNamespacedHelpers("counter"); // use counter store
 
 Vue.use(VueThreejs);
 
 @Component({
   components: {
     Cube
+  },
+  computed: {
+    ...mapGetters(["count"])
   }
 })
 export default class ThreeJSExample extends Vue {}
