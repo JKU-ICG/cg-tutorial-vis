31e5f8adcc95deb6fa5c225428ce68c2bba69036
VAISHALI-DHANOA <vaishali.dhanoa@gmail.com>
2018-08-21 16:26:54 +0200

    Destroy event added to remove the cube.

--- src/components/MyCubeControls.vue
+++ src/components/MyCubeControls.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>   
+  <div>  
+     Select scale: 
+    <select v-model="scale">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+    </select>  
+    <button @click="init">Initialize</button>
+    <button @click="animate">Animate</button>
+    <!-- <button @click="destroy">Destroy</button> -->
   </div>
 </template>
 
@@ -8,40 +17,58 @@
 import * as Three from 'three';
 
 export default {
-    data: 
-        function() {
-            return { 
-                width: 30,
-                height: 30,               
-            } 
-        },    
 
-    mounted: function(){
-        init();
-        animate();
-        var renderer, scene, camera, cube;
-         function init()
-        {
-            scene = new Three.Scene();
-            camera = new Three.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
-            renderer = new Three.WebGLRenderer( { antialias: true } );
-            var geometry = new Three.BoxGeometry(200,200,200);
-            var material = new Three.MeshBasicMaterial({ color:  0xFFFF00 });
-            cube = new Three.Mesh(geometry, material);
-            cube.name = "myCube";
-            camera.position.z = 400;        
-            scene.add(cube);              
-            renderer.setPixelRatio(window.devicePixelRatio);
-            renderer.setSize(window.innerWidth, window.innerHeight); 
-            document.body.appendChild(renderer.domElement);            
-        }
+        data:function(){
 
-        function animate(){                      
-            requestAnimationFrame(animate);
-            cube.rotation.x += 0.01;
-            //cube.rotation.y += 0.02;
-            renderer.render(scene, camera);
+            return{               
+                scale: 3,
+                renderer: new Three.WebGLRenderer( { antialias: true } ), 
+                scene: new Three.Scene(), 
+                camera: new Three.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 ), 
+                geometry: new Three.BoxGeometry(200,200,200),
+                material: new Three.MeshBasicMaterial({ color:  0xFFFF00 }),
+                cube: new Three.Mesh()
+            }            
+        },
+        
+        methods: {
+           init: function(){                
+                var finalSize = this.scale * 100;    
+                this.renderer = new Three.WebGLRenderer( { antialias: true } );
+                this.scene = new Three.Scene();
+                this.camera = new Three.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
+                this.geometry = new Three.BoxGeometry(200,200,200);
+                this.material = new Three.MeshBasicMaterial({ color:  0xFFFF00 });
+                this.geometry = new Three.BoxGeometry(finalSize, finalSize, finalSize);
+                this.cube = new Three.Mesh(this.geometry, this.material);           
+                this.cube.name = "myCube";
+                this.camera.position.z = 400;        
+                this.scene.add(this.cube);              
+                this.renderer.setPixelRatio(window.devicePixelRatio);
+                this.renderer.setSize(window.innerWidth, window.innerHeight); 
+                document.body.appendChild(this.renderer.domElement); 
+            },
+            animate: function(){
+                requestAnimationFrame(this.animate);
+                this.cube.rotation.x += 0.01;                
+                this.renderer.render(this.scene, this.camera);
+            },
+            destroy: function(){
+                this.material.dispose();
+                this.geometry.dispose();                            
+                document.body.removeChild(this.renderer.domElement);
+            }
+
+        },
+        watch: {
+            scale: function(){                
+                this.destroy();
+            }
+        },
+
+        mounted: function(){
+            this.init();
+            this.animate();
         }
-    }    
-};
+    };
 </script>
