c3aa0904aebb5ad6cf697b66fe91468f7a770b63
Holger Stitz <holger.stitz@jku.at>
2018-09-12 15:10:48 +0200

    Fix imports

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -9,7 +9,6 @@
 <script lang="ts">
 import Vue from 'vue';
 import slider from 'vue-color/src/components/Slider.vue';
-import { AbstractView } from './AbstractView.vue';
 import { createNamespacedHelpers } from 'vuex';
 import { Component, Prop } from 'vue-property-decorator';
 import { mixins } from 'vue-class-component';
