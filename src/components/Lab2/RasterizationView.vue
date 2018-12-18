b205b38c3f781c309978811627169e26e77131f8
VAISHALI-DHANOA <vaishali.dhanoa@gmail.com>
2018-12-18 12:37:30 +0100

    Cleaned up code

--- src/components/Lab2/RasterizationView.vue
+++ src/components/Lab2/RasterizationView.vue
@@ -50,15 +50,19 @@ import {
 @Component({})
 export class Rasterization extends Vue {
 
-    private renderer = new WebGLRenderer();
-    private scene = new Scene();
-    private camera = new PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
-    private bufferScene = new Scene();
-    private bufferTexture: any;
-    private boxMesh: any;
-    private screenWidth: any;
-    private screenHeight: any;
+    private screenWidth = window.innerWidth / 2;
+    private screenHeight = window.innerHeight / 2;
     private aspectRatio: any;
+
+    private renderer: any;
+    private scene: any;
+    private camera: any;
+
+    private bufferScene: any;
+    private bufferTexture: any;
+    private bufferUniforms: any;
+
+    private boxMesh: any;
 
     private mounted() {
         this.init();
@@ -67,63 +71,15 @@ export class Rasterization extends Vue {
 
     private init() {
 
-        this.screenWidth = window.innerWidth / 2;
-        this.screenHeight = window.innerHeight / 2;
         this.aspectRatio = this.screenWidth / this.screenHeight;
 
         this.camera = new PerspectiveCamera(70, this.aspectRatio, 1, 1000);
 
-        this.scene = new Scene();
-        this.scene.background = new Color(0xffffff);
-
-        // have a buffer scene, offscreen buffer
-        this.bufferScene = new Scene();
-
-        const cube = new Mesh(new BoxBufferGeometry(2, 2, 2), new MeshBasicMaterial({ color: 0xF06565 }));
-        cube.position.z = -10;
-        this.bufferScene.add(cube);
-
-        const parameters = {
-            minFilter: LinearFilter,
-            magFilter: LinearFilter,
-            format: RGBAFormat,
-            stencilBuffer: false,
-        };
-
-        this.bufferTexture = new WebGLRenderTarget(window.innerWidth, window.innerHeight, parameters);
-
-        const resValue = new Vector2(window.innerWidth, window.innerHeight);
-        resValue.multiplyScalar(window.devicePixelRatio);
-
-        const textureVal = this.bufferTexture.texture;
-
-        // calculate uniforms based on the render target
-        const uniforms1 = {
-            texture: { value: textureVal },
-            resolution: { value: resValue },
-            pixelSize: { value: 2.0 },
-        };
-
-        const boxMaterial = new ShaderMaterial({
-            uniforms: uniforms1,
-            vertexShader: document.getElementById('vertexShader')!.textContent || '',
-            fragmentShader: document.getElementById('fragmentShader')!.textContent || '',
-        });
-
-        // boxMaterial.uniforms.texture.value.wrapS = boxMaterial.uniforms.texture.value.wrapT = ClampToEdgeWrapping;
-
-        const boxGeometry = new BoxBufferGeometry(5, 5, 5);
-
-        this.boxMesh = new Mesh(boxGeometry, boxMaterial);
-
-        this.boxMesh.position.z = -10;
-
-        this.scene.add(this.boxMesh);
-
-        this.renderer.setPixelRatio(window.devicePixelRatio);
-        this.renderer.setSize(this.screenWidth, this.screenHeight);
-
-        this.$el.appendChild(this.renderer.domElement);
+        this.initRenderer();
+        this.createBufferScene();
+        this.createBufferTexture();
+        this.createBufferUniforms();
+        this.createMainScene();
     }
 
     private animate() {
@@ -139,6 +95,77 @@ export class Rasterization extends Vue {
         this.renderer.render(this.scene, this.camera);
     }
 
+    private initRenderer() {
+
+        this.renderer = new WebGLRenderer();
+
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        this.renderer.setSize(this.screenWidth, this.screenHeight);
+
+        this.$el.appendChild(this.renderer.domElement);
+    }
+
+    private createBufferScene() {
+
+        this.bufferScene = new Scene();
+
+        const cubeGeometry = new BoxBufferGeometry(2, 2, 2);
+
+        const cubeMaterial = new MeshBasicMaterial({ color: 0xF06565 });
+
+        const cube = new Mesh(cubeGeometry, cubeMaterial);
+
+        cube.position.z = -10;
+
+        this.bufferScene.add(cube);
+    }
+
+    private createBufferTexture() {
+
+        const parameters = {
+            minFilter: LinearFilter,
+            magFilter: LinearFilter,
+            format: RGBAFormat,
+            stencilBuffer: false,
+        };
+
+        this.bufferTexture = new WebGLRenderTarget(window.innerWidth, window.innerHeight, parameters);
+    }
+
+    private createBufferUniforms() {
+
+        const resValue = new Vector2(window.innerWidth, window.innerHeight)
+            .multiplyScalar(window.devicePixelRatio);
+
+        const textureVal = this.bufferTexture.texture;
+
+        this.bufferUniforms = {
+            texture: { value: textureVal },
+            resolution: { value: resValue },
+            pixelSize: { value: 2.0 },
+        };
+    }
+
+    private createMainScene() {
+
+        this.scene = new Scene();
+
+        this.scene.background = new Color(0xffffff);
+
+        const boxMaterial = new ShaderMaterial({
+            uniforms: this.bufferUniforms,
+            vertexShader: document.getElementById('vertexShader')!.textContent || '',
+            fragmentShader: document.getElementById('fragmentShader')!.textContent || '',
+        });
+
+        const boxGeometry = new BoxBufferGeometry(5, 5, 5);
+
+        this.boxMesh = new Mesh(boxGeometry, boxMaterial);
+
+        this.boxMesh.position.z = -10;
+
+        this.scene.add(this.boxMesh);
+    }
 }
 
 export default Rasterization;
